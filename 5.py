7f5927fd05676d49d07f69c15d50a1598f050ce8
Wayrone1 <112934802+Wayrone1@users.noreply.github.com>
2022-11-23 15:26:06 +0300

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,24 +1,16 @@
-"""Сложность данной функции O(n).
-        Args:
-            matrix (List[List[int]]): matrix where we search squares.
-        Returns:
-            int: amount of squares in matrix.
-        """
+"""Сложность данной функции O(n)."""
 
 from typing import List
+
+
 class Solution:
     def getDescentPeriods(self, prices: List[int]) -> int:
-        lst = []
-        result = 0
-        cur = 1
-        for i in range(1, len(prices)):
-            if prices[i-1] - prices[i] == 1:
-                cur += 1
-            else:
-                lst.append(cur)
-                cur = 1     
-        lst.append(cur)
-        for j in lst:
-            result += ((1 + j)*j) // 2
-            
-        return result
+       
+        pr = len(prices)
+        d = [1]*pr
+        f = 1
+        for i in range(1, pr):
+            if prices[i-1]-prices[i] == 1:
+                d[i] = d[i-1]+1
+            f += d[i]
+        return f
