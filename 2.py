339f497e13fa3f9b099525c3c70678e00d88bfaa
Wayrone1 <112934802+Wayrone1@users.noreply.github.com>
2022-11-23 15:15:42 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,26 +1,18 @@
-from typing import List
-class Solution:
-    """Сложность данной функции O(n*m).
+
+    """Сложность данной функции O(n+1)
         Args:
-            matrix (List[List[int]]): matrix where we search squares.
+            n (int): list's length.
         Returns:
-            int: amount of squares in matrix.
-        """
-    def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
-        row, col = len(grid), len(grid[0])
-        def goal(arr, i,j,k,count):  # мне нужно вернуть count (после обновления), поэтому моя рекурсивная функция должна возвращать count
-            if i>=row or j>= col:
-                return 0
-            if arr[i][j]==1:  
-                return 0
-            if i==row-1 and j==col-1:   #цель достигнута, увеличьте количество на 1
-                count+=1
-                return count
-				# поскольку мы будем двигаться либо вниз, либо вправо
-                # , мы не столкнемся с какой-либо проблемой зацикливания в этой задаче
-            if (i,j) not in k:   #чтобы избежать повторного вычисления ранее посещенных путей
-                count = goal(arr,i+1,j,k,count)+goal(arr, i,j+1,k,count)
-                k[(i,j)] = count
-            return k[(i,j)]
-        return goal(grid, 0,0,{}, 0)
-		
+            int: max element in this list.
+        """     
+class Solution:
+    def getMaximumGenerated(self, n: int) -> int:
+        gen = [0] * (n+1)
+        if len(gen) >= 2:
+            gen[1] = 1
+        for i in range(2,n+1):
+            if i % 2 == 0:
+                gen[i] = gen[i //2]
+            else:
+                gen[i] = gen[i//2] + gen[i//2 + 1]
+        return max(gen)        
