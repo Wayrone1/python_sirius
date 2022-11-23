cf73320552e7d2415be5fdf4829ac397ed1dbf87
Wayrone1 <112934802+Wayrone1@users.noreply.github.com>
2022-11-23 15:21:02 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -2,18 +2,13 @@ from typing import List
 
 class Solution:
     """Сложность данной функции O(n*m).
-        Args:
-            matrix (List[List[int]]): matrix where we search squares.
-        Returns:
-            int: amount of squares in matrix.
         """
     def maxProfit(self, prices: List[int]) -> int:
-        # Инициализируйте максимальную прибыль
         maximumProfit = 0
-        # Пройдите весь элемент через цикл
+        # Проходит элемент через цикл
         for i in range(1, len(prices)):
-            # проверьте, больше ли цена на i
+            # Проверяет больше ли цена на i
             if prices[i] > prices[i-1]:
-                # Добавьте разницу к прибыли и увеличьте ее
+                # Добавляет разницу к прибыли и увеличивает ее
                 maximumProfit += prices[i] - prices[i-1]
-        return maximumProfit        # Верните максимальную прибыль
+        return maximumProfit        # Возращает максимальную прибыль
